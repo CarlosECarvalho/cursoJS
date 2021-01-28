5db0c1e1970dd7a0aef7167ebecac40ff83517bd
CarlosECarvalho <krlosnight@gmail.com>
2021-01-28 00:03:31 -0300

    js module

---
+++ Conditions/exer008.js
@@ -0,0 +1,7 @@
+//console.log('O comando funcionou')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km por hora`);
+
+console.log(`Dirija com atenção`);
+console.log(`Use sinto de segurança`);

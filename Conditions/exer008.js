7e1728ce21ad11dc438d3e7c12201d1589f40d54
CarlosECarvalho <krlosnight@gmail.com>
2021-01-28 20:39:46 -0300

    JS exe

--- Conditions/exer008.js
+++ Conditions/exer008.js
@@ -1,4 +1,4 @@
-/*console.log('O comando funcionou')
+//console.log('O comando funcionou')
 
 var vel = 15
 console.log(`A velocidade do seu carro é ${vel} km por hora`);
@@ -6,12 +6,5 @@ if (vel > 60){ //CONDICAO SIMPLES
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
 }
 console.log(`Dirija com atenção`);
-console.log(`Use sinto de segurança`);*/
+console.log(`Use sinto de segurança`);
 
-var pais = 'Brasil'
-console.log(`Vivendo em ${pais}`);
-if(pais != 'Brasil'){
-    console.log('Você é gringo e paga mais caro.')
-}else {
-    console.log('Você é brasileiro')
-}

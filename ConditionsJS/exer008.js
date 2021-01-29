7266584e853b4d66f8f15acc62e1cdfc9138b37f
CarlosECarvalho <krlosnight@gmail.com>
2021-01-28 22:09:45 -0300

    exejs

--- ConditionsJS/exer008.js
+++ ConditionsJS/exer008.js
@@ -2,7 +2,7 @@
 
 var vel = 15
 console.log(`A velocidade do seu carro é ${vel} km por hora`);
-if (vel > 60){ //CONDICAO SIMPLES
+if (vel > 60){ //CONDICAO SIMPLES, deve ter uma expressão para que possa ser testado
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
 }
 console.log(`Dirija com atenção`);
